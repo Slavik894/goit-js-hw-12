2b634f48871067316e6ec7e38b3732b57c5e9566
Slavik <slavik1058@gmail.com>
2025-06-03 01:48:25 +0200

    functionality of Load more button

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,16 +2,24 @@ import axios from 'axios';
 
 const api_key = "50385354-3e4bea1f358fcf603a5420cc4";
 const baseURL = "https://pixabay.com/api/";
+let page = 1;
+export const per_page = 15;
+
+
+
 axios.defaults.baseURL= baseURL;
 
-export async function getImagesByQuery(query){
+export async function getImagesByQuery(query, page){
     const response = axios.get(`${baseURL}`, {
         params:{
             key: api_key,
             q: query,
             image_type: 'photo',
             orientation: 'horizontal',
-            safesearch: true
+            safesearch: true,
+            page: page,
+            per_page: per_page
+
         }
     });
     return response;
--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
-import { getImagesByQuery } from './js/pixabay-api.js';
+import { getImagesByQuery, per_page } from './js/pixabay-api.js';
 import {clearGallery, createGallery, showLoader, hideLoader} from './js/render-functions.js';
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector('.form');
+const load_more_btn = document.querySelector(".load-more-btn");
+
+let userValue;
+let currentPage = 1;
+let totalPages = 0;
+
+
 
 form.addEventListener('submit', async event => {
   event.preventDefault();
-  const query = form.elements['search-text'].value.trim();
-  if (!query) return;
+   userValue = form.elements['search-text'].value.trim();
+  if (!userValue) return;
 
+  currentPage = 1;
   clearGallery();
   showLoader();
 
     try{
-      const response = await getImagesByQuery(query)
+      const response = await getImagesByQuery(userValue, currentPage)
       const images = response.data.hits;
+      const totalHits = response.data.totalHits;
       if (images.length > 0) {
         createGallery(images);
+
+        totalPages = Math.ceil(totalHits / per_page);
+        if(currentPage<totalPages){
+          load_more_btn.classList.add('is-active');
+        }
+ 
       }
       else{
         iziToast.show({
@@ -28,7 +43,10 @@ form.addEventListener('submit', async event => {
       }
     }
     catch(error){
-      iziToast.show({message: error,  class: 'custom-toast'});
+      iziToast.show({
+        message: error,  
+        class: 'custom-toast'
+      });
     }  
     finally{
       hideLoader();
@@ -36,4 +54,29 @@ form.addEventListener('submit', async event => {
    form.elements['search-text'].value = '';
     
   
+});
+
+load_more_btn.addEventListener('click', async ()=>{
+  showLoader();
+  try{
+    currentPage++;
+    const response = await getImagesByQuery(userValue, currentPage);
+    const images = response.data.hits;
+
+    if(images.length>0){
+      createGallery(images);
+    }
+    if(currentPage< totalPages){
+      load_more_btn.classList.add('is-active');
+    }
+  }
+  catch(error){
+    iziToast.show({
+      message: "We're sorry, but you've reached the end of search results."
+    });
+  }
+  finally{
+    hideLoader();
+  }
+
 });
